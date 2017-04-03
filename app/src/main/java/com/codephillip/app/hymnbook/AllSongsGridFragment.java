201682268baaafc994d949c3328378938c76c527
codephillip <codephillip@gmail.com>
2017-04-03 18:05:48 +0300

    created instance of gridFragment

--- app/src/main/java/com/codephillip/app/hymnbook/AllSongsGridFragment.java
+++ app/src/main/java/com/codephillip/app/hymnbook/AllSongsGridFragment.java
@@ -13,7 +13,11 @@ import android.view.ViewGroup;
 import com.codephillip.app.hymnbook.adapters.SongGridAdapter;
 import com.codephillip.app.hymnbook.models.Hymn;
 import com.codephillip.app.hymnbook.models.HymnDatabase;
+import com.codephillip.app.hymnbook.provider.hymntable.HymntableCursor;
+import com.codephillip.app.hymnbook.provider.hymntable.HymntableSelection;
 import com.codephillip.app.hymnbook.utilities.Utils;
+
+import java.util.ArrayList;
 
 /**
  * Created by codephillip on 31/03/17.
@@ -27,11 +31,24 @@ public class AllSongsGridFragment extends Fragment implements SongGridAdapter.It
     public AllSongsGridFragment() {
     }
 
+    public static AllSongsGridFragment newInstance(boolean isFavorite) {
+        Log.d(TAG, "newInstance: " + isFavorite);
+        AllSongsGridFragment fragment = new AllSongsGridFragment();
+        Bundle args = new Bundle();
+        args.putBoolean(Utils.IS_FAVORITE, isFavorite);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.fragment_all_songs_grid, container, false);
+
+        boolean showFavoriteScreen = getArguments().getBoolean(Utils.IS_FAVORITE, false);
+        updateSingletonHymns(showFavoriteScreen);
+
         // data to populate the RecyclerView with
         String[] data = new String[HymnDatabase.hymns.getHymnArrayList().size()];
         int counter = 0;
@@ -52,8 +69,30 @@ public class AllSongsGridFragment extends Fragment implements SongGridAdapter.It
     @Override
     public void onItemClick(View view, int position) {
         Log.i("TAG", "You clicked number " + adapter.getItem(position) + ", which is at cell position " + position);
-        Utils.position = Integer.parseInt(adapter.getItem(position));
+        Utils.position = Integer.parseInt(adapter.getItem(position - 1));
         Utils.isSongActivityActive = false;
         startActivity(new Intent(getContext(), SongActivity.class));
+    }
+
+    private void updateSingletonHymns(boolean showFavoriteScreen) {
+        ArrayList<Hymn> hymnArrayList = new ArrayList<>();
+        HymntableCursor cursor = queryHymnTable(showFavoriteScreen);
+        if (cursor.moveToFirst()) {
+            do {
+                hymnArrayList.add(new Hymn(cursor.getNumber(), cursor.getTitle(), cursor.getContent(), cursor.getCategory(), cursor.getId(), cursor.getLike()));
+            } while (cursor.moveToNext());
+        }
+        HymnDatabase.hymns.setHymnArrayList(hymnArrayList);
+        Log.d(TAG, "onCreateView: ###" + HymnDatabase.hymns.getHymnArrayList().size());
+    }
+
+    private HymntableCursor queryHymnTable(boolean showFavoriteScreen) {
+        HymntableCursor cursor;
+        if (showFavoriteScreen) {
+            cursor = new HymntableSelection().like(true).query(getContext().getContentResolver());
+        } else {
+            cursor = new HymntableSelection().query(getContext().getContentResolver());
+        }
+        return cursor;
     }
 }
